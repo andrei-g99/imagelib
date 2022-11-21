9f26d9e1e4d3ed5dad0fc16e79c7052752cbe91c
Andrei G <76668607+SirAnders@users.noreply.github.com>
2022-11-21 15:29:00 +0100

    Add files via upload

---
+++ imagelib/imagelib/imagelib.cpp
@@ -0,0 +1,94 @@
+// imagelib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "img.h"
+
+// TODO: This is an example of a library function
+namespace img {
+
+	/*  COLOR CLASS DEFINITIONS  */
+	color::color(uint8_t red, uint8_t green, uint8_t blue) {
+		r = red;
+		g = green;
+		b = blue;
+
+	}
+
+	void color::set_r(uint8_t new_r) {
+		r = new_r;
+	}
+
+	void color::set_g(uint8_t new_g) {
+		g = new_g;
+	}
+
+	void color::set_b(uint8_t new_b) {
+		b = new_b;
+	}
+
+	uint8_t color::get_r() {
+		return r;
+	}
+
+	uint8_t color::get_g() {
+		return g;
+	}
+
+	uint8_t color::get_b() {
+		return b;
+	}
+
+	/*  IMAGE CLASS DEFINITIONS  */
+
+	image::image(unsigned int width, unsigned int height, color fill) {
+		m_width = width;
+		m_height = height;
+		//filled background image constructor
+		for (int i = 0; i < (int)height; i++) {
+			vector<color> temp_row;
+			for (int j = 0; j < (int)width; j++) {
+				temp_row.push_back(fill);
+			}
+			map.push_back(temp_row);
+			temp_row.clear();
+		}
+	}
+
+	unsigned int image::get_width() {
+		return m_width;
+	}
+	unsigned int image::get_height() {
+		return m_height;
+	}
+
+	bool image::save_ppm(std::string name) {
+		//saves image as PPM file at the specified path dir, returns false if failed
+		std::string temp;
+		temp = name + ".ppm";
+		std::fstream fs;
+		fs.open(temp, std::fstream::out);
+		if (fs.is_open()) {
+			fs << "P6\n";
+			fs << m_width << " " << m_height << "\n";
+			fs <<255<<"\n";
+
+			//write pixel map
+			for (auto& y : map) {
+				for (auto& x : y) {
+					fs << (char)x.get_g() << (char)x.get_b() << (char)x.get_r();
+				}
+			}
+
+		}
+		else {
+			std::cout << temp << " couldn't be created.\n";
+			return false;
+		}
+
+		fs.close();
+
+		return true;
+	}
+}
---
+++ imagelib/imagelib/img.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+using std::vector;
+
+namespace img {
+
+	class color {
+	private:
+		uint8_t r;
+		uint8_t g;
+		uint8_t b;
+
+	public:
+		color(uint8_t red, uint8_t green, uint8_t blue);
+
+		void set_r(uint8_t new_r);
+		void set_g(uint8_t new_g);
+		void set_b(uint8_t new_b);
+
+		uint8_t get_r();
+		uint8_t get_g();
+		uint8_t get_b();
+
+	};
+
+	class image {
+	private:
+		vector<vector<color>> map;
+		unsigned int m_width;
+		unsigned int m_height;
+
+	public:
+		image(unsigned int width, unsigned int height, color fill);
+
+		unsigned int get_width();
+		unsigned int get_height();
+		bool save_ppm(std::string path);
+	};
+}
